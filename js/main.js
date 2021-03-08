a457cf411372d0c3ed80beed508feb2cc7ada890
Cancermaker <52158699+Cancermaker@users.noreply.github.com>
2021-03-08 17:54:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,26 +10,29 @@ const M_audio1 = new Audio('/audio/Malkuth/Malkuth battle1.mp3');
 const M_audio2 = new Audio('/audio/Malkuth/Malkuth battle2.mp3');
 const M_audio3 = new Audio('/audio/Malkuth/Malkuth battle 3.mp3');
 
+const R_audio1 = new Audio('/audio/kether/Kether battle1.mp3');
+const R_audio2 = new Audio('/audio/kether/Kether battle2.mp3');
+
 const floor_name= {Malkuth, Yesod, Hod, Roland};
 
 
 function phase1() {
-    Y_audio1.play();
-    Y_audio1.loop = true;
-    Y_audio2.load();
-    Y_audio3.load();
+    M_audio1.play();
+    M_audio1.loop = true;
+    M_audio2.load();
+    M_audio3.load();
 }
 function phase2() {
 
-    Y_audio2.play();
-    Y_audio1.loop = true;
-    Y_audio1.load();
-    Y_audio3.load();
+    M_audio2.play();
+    M_audio1.loop = true;
+    M_audio1.load();
+    M_audio3.load();
 }
 function phase3() {
 
-    Y_audio3.play();
-    Y_audio1.loop = true;
-    Y_audio2.load();
-    Y_audio1.load();
+    M_audio3.play();
+    M_audio1.loop = true;
+    M_audio2.load();
+    M_audio1.load();
 }

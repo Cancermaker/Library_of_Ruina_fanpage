f69bc98540dc946735f3bc090edf273a93fb3c21
unknown <52158699+Cancermaker@users.noreply.github.com>
2021-03-09 17:45:14 +0900

    [add] ㅈ망해서 재설계 예정

--- js/main.js
+++ js/main.js
@@ -13,26 +13,100 @@ const M_audio3 = new Audio('/audio/Malkuth/Malkuth battle 3.mp3');
 const R_audio1 = new Audio('/audio/kether/Kether battle1.mp3');
 const R_audio2 = new Audio('/audio/kether/Kether battle2.mp3');
 
-const floor_name= {Malkuth, Yesod, Hod, Roland};
+const floor_name= ["Malkuth", "Yesod", "Hod", "Roland"];
+let floor = -1;
+
+
+let audio1 ;
+let audio2 ;
+let audio3 ;
+
+function move_left() {
+    floor = floor - 1;
+    sound_remover()
+    alert(floor)
+    now_floor_music(floor)
+}
+function move_right() {
+    floor = floor + 1;
+    alert(floor)
+    now_floor_music(floor)
+}
+function sound_remover() {
+        audio1.load(); audio2.load(); audio3.load();
+    alert("1")
+
+}
+function now_floor_music(fl) {
+
+    switch (fl) {
+        case 0:
+            audio1 = R_audio1;
+            audio2 = R_audio2;
+            audio3 = null;
+            break;
+        case 1:
+            audio1 = M_audio1;
+            audio2 = M_audio2;
+            audio3 = M_audio3;
+            break;
+        case 2:
+            audio1 = Y_audio1;
+            audio2 = Y_audio2;
+            audio3 = Y_audio3;
+            break;
+        case 3:
+            audio1 = H_audio1;
+            audio2 = H_audio2;
+            audio3 = H_audio3;
+            break;
+        default:
+            audio1 = R_audio1;
+            audio2 = R_audio2;
+            audio3 = null;
+            break;
+    }
+}
+
+let phase;
+let background_img = document.getElementsByTagName("body");
+let floor_now = document.getElementsByClassName("now_floor");
+let start_s = document.getElementsByClassName("start_1")
 
 
 function phase1() {
-    M_audio1.play();
-    M_audio1.loop = true;
-    M_audio2.load();
-    M_audio3.load();
+    phase = 1;
+    audio_play1(phase);
 }
 function phase2() {
-
-    M_audio2.play();
-    M_audio1.loop = true;
-    M_audio1.load();
-    M_audio3.load();
+    phase = 2;
+    audio_play2(phase);
 }
 function phase3() {
+    phase = 3;
+    audio_play3(phase);
+}
 
-    M_audio3.play();
-    M_audio1.loop = true;
-    M_audio2.load();
-    M_audio1.load();
+function audio_play1(p) {
+    audio1.play();
+    audio1.loop = true;
+    audio2.load();
+    audio3.load();
+
+}
+function audio_play2(p) {
+    audio2.play();
+    audio2.loop = true;
+    audio1.load();
+    audio3.load();
+
+}
+function audio_play3(p) {
+    audio3.play();
+    audio3.loop = true;
+    audio2.load();
+    audio1.load();
+
+}
+function start() {
 }
